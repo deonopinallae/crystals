3b356a4ae5f18e2225134c9087d523d727724d08
deonopinallae <deonopinallae@yandex.ru>
2026-02-06 18:35:17 +0300

    fix(header): improve header layout and styles

--- src/shared/ui/layout/header/home-header.tsx
+++ src/shared/ui/layout/header/home-header.tsx
@@ -1,21 +1,21 @@
 'use client'
+import Link from 'next/link'
 import { SearchIcon, PyramidIcon } from '../../icons'
 import styles from './styles.module.scss'
 
 export const HomeHeader = () => {
 	return (
 		<>
-			<header className={`${styles.header} py-[1.13rem] lg:py-[1.88rem] container z-2`}>
-				<button className="flex flex-col gap-[7px]">
+			<header className={`${styles.header} py-[1.13rem] lg:py-[1.88rem] z-20 w-full`}>
+				<button className="flex flex-col gap-[7px]" aria-label="Menu">
 					{[...Array(3)].map((_, i) => (
 						<div key={i} className="w-[28px] h-[2px] bg-black"></div>
 					))}
-					
 				</button>
-				<div className="font-bricolage uppercase md:block hidden">Free shipping on orders over $75</div>
+				<Link href="/" className="font-bricolage uppercase md:block hidden text-sm">Free shipping on orders over $75</Link>
 				<div className='flex gap-[1rem] items-center'>
-					<button><SearchIcon /></button>
-					<button><PyramidIcon /></button>
+					<button aria-label="Search"><SearchIcon /></button>
+					<Link href="/cart" aria-label="Shopping cart"><PyramidIcon /></Link>
 				</div>
 			</header>
 		</>
