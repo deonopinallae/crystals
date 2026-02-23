eb3f9a25137ada72b5c065a6d321dc5e42916ea5
deonopinallae <deonopinallae@yandex.ru>
2026-02-23 23:42:10 +0300

    style(shop-by-intention): remove love fill and soften hover glow

--- src/shared/ui/icons/intention-love-icon.tsx
+++ src/shared/ui/icons/intention-love-icon.tsx
@@ -17,7 +17,6 @@ export const IntentionLoveIcon = ({
 		xmlns="http://www.w3.org/2000/svg"
 		className={className}
 	>
-		<circle cx="52.5313" cy="52" r="52" fill="#F0DCF1" />
 		<path
 			d="M52.5313 103C80.9912 103 104.063 80.1665 104.063 52C104.063 23.8335 80.9912 1 52.5313 1C24.0713 1 1 23.8335 1 52C1 80.1665 24.0713 103 52.5313 103Z"
 			stroke={color}
