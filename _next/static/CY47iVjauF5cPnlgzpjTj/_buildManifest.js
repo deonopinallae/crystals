dbdc5cf7bc4278e1002defd7cf3ed20bfeae5e15
deonopinallae <deonopinallae@yandex.ru>
2026-03-01 15:58:25 +0300

    Updates

--- _next/static/CY47iVjauF5cPnlgzpjTj/_buildManifest.js
+++ _next/static/CY47iVjauF5cPnlgzpjTj/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/crystals//_next/:path+",
-        "destination": "/crystals/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
