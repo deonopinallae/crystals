6c1d84e54070f0142a72bda1c26b524598ab3bf0
deonopinallae <deonopinallae@yandex.ru>
2026-03-06 13:25:33 +0300

    Fix layout exports for Next.js build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from 'next'
 import { Bricolage_Grotesque, Roboto, Newsreader } from 'next/font/google'
 import { AppColumn } from '@/shared/ui/layout/app-column'
-import '@/styles/styles.scss'
+import '../styles/styles.scss'
 
-export const bricolage = Bricolage_Grotesque({
+const bricolage = Bricolage_Grotesque({
 	subsets: ['latin'],
 	variable: '--font-bricolage',
 })
-export const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
-export const newsreader = Newsreader({
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const newsreader = Newsreader({
 	subsets: ['latin'],
 	variable: '--font-newsreader',
 })
